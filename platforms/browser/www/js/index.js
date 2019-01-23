1efbbb610545aa9c3e4146d874a06040e80942ea
mnb-dade <mohaballi@dadesystems.com>
2019-01-22 23:28:58 -0500

    Supply required params

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,43 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-  // Application Constructor
+const app = {
+  
   initialize: function() {
-    document.addEventListener(
-      'deviceready', 
-      this.onDeviceReady.bind(this), 
-      false
-    );
-  },
-
-  // deviceready Event Handler
-  //
-  // Bind any cordova events here. Common events are:
-  // 'pause', 'resume', etc.
-  onDeviceReady: function() {
-    this.bindPayButton();
-  },
-
-  bindPayButton: function() {
     document
       .getElementById('pay-by-check')
-      .addEventListener("click", this.payButtonClicked);
+      .addEventListener('click', this.payButtonClicked);
   },
 
   payButtonClicked: function(event) {
@@ -46,27 +12,38 @@ var app = {
     const SERVICE_URL = 'http://192.168.1.122:3000/mobile_payments/new';
     const SUCCESS_URL = 'https://www.company.com/success.html';
     const CANCEL_URL = 'https://www.company.com/cancel.html';
+    
+    const params = {
+      api_key: '2b4d57bb43bf6bfe7a600ecb1e7cf1a5',
+      external_user_id: '123',
+      amount: 50,
+      success_url: SUCCESS_URL,
+      cancel_url: CANCEL_URL
+    };
+
+    const paramString = Object.keys(params).map(function(key) {
+      return key + '=' + params[key];
+    }).join('&');
 
     const ref = cordova.InAppBrowser.open(
-      `${SERVICE_URL}?success_url="${SUCCESS_URL}"&cancel_url="${CANCEL_URL}"`, 
+      `${SERVICE_URL}?${paramString}`, 
       '_blank', 
       'location=no'
     );
 
     ref.addEventListener('loadstart', function(event){
+      if (!event.url.startsWith(SERVICE_URL)) ref.close();
       if (event.url.startsWith(SUCCESS_URL)) handleSuccess(event);
       else if (event.url.startsWith(CANCEL_URL)) handleCancel(event);
     });
 
     function handleSuccess(event) {
-      ref.close();
       setTimeout(function(){
         alert('Payment Success!');
       }, 100);
     }
 
     function handleCancel(event) {
-      ref.close();
       setTimeout(function(){
         alert('Payment Canceled!');
       }, 100);
--- www/js/index.js
+++ www/js/index.js
@@ -12,9 +12,21 @@ const app = {
     const SERVICE_URL = 'http://192.168.1.122:3000/mobile_payments/new';
     const SUCCESS_URL = 'https://www.company.com/success.html';
     const CANCEL_URL = 'https://www.company.com/cancel.html';
+    
+    const params = {
+      api_key: '2b4d57bb43bf6bfe7a600ecb1e7cf1a5',
+      external_user_id: '123',
+      amount: 50,
+      success_url: SUCCESS_URL,
+      cancel_url: CANCEL_URL
+    };
+
+    const paramString = Object.keys(params).map(function(key) {
+      return key + '=' + params[key];
+    }).join('&');
 
     const ref = cordova.InAppBrowser.open(
-      `${SERVICE_URL}?success_url="${SUCCESS_URL}"&cancel_url="${CANCEL_URL}"`, 
+      `${SERVICE_URL}?${paramString}`, 
       '_blank', 
       'location=no'
     );
