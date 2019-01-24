d854c88a156b85cd4ce3e4966c68a04018ddf560
mnb-dade <mohaballi@dadesystems.com>
2019-01-23 22:17:42 -0500

    Capture with cordova plugin

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,13 +1,13 @@
 window.addEventListener("message", receiveMessage, false);
 
-function payNow() {
-  const SERVICE_URL = 'http://192.168.1.122:3000/mobile_payments/new';
-  
+const SERVICE_URL = 'http://192.168.1.122:3000/mobile_payments/new';
+
+function payNow() {  
   const params = {
     api_key: '2b4d57bb43bf6bfe7a600ecb1e7cf1a5',
     external_user_id: '123',
-    amount: 555.50,
-    // parent_capture_enabled: true
+    amount: 555.55,
+    parent_capture_enabled: true
   };
 
   $('#payment-iframe')
@@ -17,11 +17,35 @@ function payNow() {
   $('#home-screen').hide();
 }
 
+// 
+// Dade Events
+// 
 function receiveMessage(event){
-  if (event.origin !== "http://192.168.1.122:3000") return;
-  if (event.data.imageRequested) handleImageRequest(event.data.imageRequested);
+  if (!SERVICE_URL.startsWith(event.origin)) return;
+  if (event.data.imageRequested) handleImageRequest(event);
   else if (event.data.paymentSuccess) handlePaymentSuccess();
   else if (event.data.paymentCanceled) handlePaymentCanceled();
+}
+
+function handleImageRequest(event) {
+  const imageType = event.data.imageRequested; 
+
+  function onSuccess(imageData) {
+    const imageDataUrl = "data:image/jpeg;base64," + imageData;
+    event.source.postMessage({
+      imageType,
+      imageDataUrl
+    }, '*');
+  }
+  
+  function onFail(message) {
+    alert('Failed because: ' + message);
+  }
+
+  navigator.camera.getPicture(onSuccess, onFail, { 
+    quality: 25,
+    destinationType: Camera.DestinationType.DATA_URL
+  });
 }
 
 function handlePaymentSuccess(){
@@ -36,22 +60,17 @@ function handlePaymentCanceled() {
   showAlert('Payment was canceled!');
 }
 
-function showAlert(message){
-  setTimeout(function(){ 
-    alert(message);
-  }, 100);
-}
-
-function handleImageRequest(imageType) {
-  const imageDataUrl = `data:image/png,${imageType}_base64_placeholder`;
-  event.source.postMessage({
-    imageType,
-    imageDataUrl
-  }, '*');
-}
-
+// 
+// Util functions
+// 
 function paramsToString(params){
   return Object.keys(params).map(function(key) {
     return key + '=' + params[key];
   }).join('&');
+}
+
+function showAlert(message){
+  setTimeout(function(){ 
+    alert(message);
+  }, 100);
 }
